3c6332c8da1184f4be0fd0683ad294711e10f7a5
Dorion Mitchell <dorionmitchell@Dorions-MBP.local>
2023-08-04 15:24:11 -0400

    Added one more test around redirection

--- tests_app.py
+++ tests_app.py
@@ -21,5 +21,10 @@ class ConversionTestCase(TestCase):
 
             self.assertEqual(res.status_code, 200)
             self.assertIn('<p id="error">Not a valid Code: usd Please visit https://www.iban.com/currency-codes for a full list of Currency Codes</p>\n      \n        <p id="error">Not a valid Code: inr Please visit https://www.iban.com/currency-codes for a full list of Currency Codes</p>', html)
-        
-    
+
+    def test_redirection(self):
+        with app.test_client() as client:
+            res = client.post('/conversion', data={'currcode_from': 'usd', 'currcode_to': 'inr', 'amount':1})
+
+            self.assertEqual(res.status_code, 302)
+            self.assertEqual(res.location, "/")
