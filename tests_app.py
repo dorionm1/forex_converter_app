190f007ff04e34e27ad4cd865460064d5569b4ff
Dorion Mitchell <dorionmitchell@Dorions-MBP.local>
2023-08-04 15:55:01 -0400

    added testing config to tests_app.py filr

--- tests_app.py
+++ tests_app.py
@@ -2,6 +2,9 @@ from app import *
 from unittest import TestCase
 from flask import session
 
+app.config['TESTING'] = True
+app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
+
 class ConversionTestCase(TestCase):
     """Unit test for Conversion App"""
 
